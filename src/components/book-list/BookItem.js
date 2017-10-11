443849000592eb60e2aec3f15bc32ef4fa4f6946
Igor Ilić <i.ilic@proway-fs.com>
2017-10-11 10:22:59 +0200

    Added interaction for child component

--- src/components/book-list/BookItem.js
+++ src/components/book-list/BookItem.js
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 const BookItem = (props) => {
 
   return (
-    <li className="list-group-item">{props.book.name}</li>
+    <li className="list-group-item" onClick={() => props.clickHandler(props.book)}>
+      {props.book.name}
+    </li>
   );
 };
 
 BookItem.propTypes = {
-  book: PropTypes.object.isRequired
+  book: PropTypes.object.isRequired,
+  clickHandler: PropTypes.func.isRequired
 };
 
 export default BookItem;
--- src/components/book-list/BookList.js
+++ src/components/book-list/BookList.js
@@ -1,18 +1,32 @@
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
 import BookItem from './BookItem';
+import BookDetails from '../book-details/BookDetails';
 
 class BookList extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      showDetails: false,
+      selectedBook: null
+    };
+
+    this.handleBook = this.handleBook.bind(this);
+  }
+
+  handleBook(book) {
+    this.setState({showDetails: true, selectedBook: book});
   }
 
   render() {
     const {books} = this.props;
     const booksIteration = books.map((book) => {
       return (
-        <BookItem key={book.id} book={book} />
+        <BookItem key={book.id} book={book} clickHandler={(book) => {
+          this.setState({showDetails: true, selectedBook: book});
+        }}/>
       );
     });
     return (
@@ -20,6 +34,10 @@ class BookList extends Component {
         <ul className="list-group">
           {booksIteration}
         </ul>
+        {this.state.showDetails
+            ? (<BookDetails book={this.state.selectedBook} />)
+            : ''
+        }
       </div>
     );
   }
